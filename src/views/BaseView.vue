c75e683c6b86e1142d4e28d5daade26bd3c4da0b
Johan Schiff <johan@radkompaniet.se>
2020-11-19 11:42:51 +0100

    BaseView and refactored api initialization

---
+++ src/views/BaseView.vue
@@ -0,0 +1,28 @@
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Base view',
+  computed: {
+    ...mapState(['isAuthenticated'])
+  },
+  methods: {
+    initialize () {},
+    logout () {}
+  },
+  watch: {
+    isAuthenticated (value) {
+      if (value) {
+        this.initialize()
+      } else {
+        this.logout()
+      }
+    }
+  },
+  created () {
+    if (this.isAuthenticated) {
+      this.initialize()
+    }
+  }
+}
+</script>
