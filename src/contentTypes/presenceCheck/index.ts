6cde988e314a56bcf9660d401fdef65ffa9884cc
Johan Schiff <johan@radkompaniet.se>
2021-03-08 09:38:04 +0100

    type mismatch for presence

--- src/contentTypes/presenceCheck/index.ts
+++ src/contentTypes/presenceCheck/index.ts
@@ -3,5 +3,5 @@ import ContentType from '../ContentType'
 import { PresenceCheck } from '../types'
 
 export default new ContentType<PresenceCheck>({
-  channelName: 'presence_system'
+  channelName: 'presence_check'
 })
