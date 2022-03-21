cf88fc86398cbba95d9b7894972497e2997bb534
Robin Harms Oredsson <robin@betahaus.net>
2022-03-21 09:54:10 +0100

    Build with version

--- build_and_publish.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-npm run build
-docker build . -t voteit/voteit4frontend:latest
-docker push voteit/voteit4frontend:latest
---
+++ build_dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+if [ $# -lt 1 ]; then
+  echo "Usage: build_dev.sh <version>"
+  exit
+fi
+VERSION="$1"
+BACKEND_VERSION="latest"
+docker build . -t voteit/voteit4frontend:$VERSION --progress plain --build-arg BACKEND_VERSION=$BACKEND_VERSION
+echo "If everything worked: "
+echo "docker push voteit/voteit4frontend:$VERSION"
