00d3bbe3d2821bfa7122e532a80a34103e032f4d
Johan Schiff <johan@radkompaniet.se>
2022-04-26 16:57:11 +0200

    vuetify beta.1 adaptations

--- src/modules/speakerLists/SpeakerListsView.vue
+++ src/modules/speakerLists/SpeakerListsView.vue
@@ -68,7 +68,7 @@
             <Moment in-seconds ordinary :date="currentSpeaker.started" />
           </p>
         </v-sheet>
-        <v-list v-if="annotatedSpeakerQueue.length" density="comfortable" color="background">
+        <v-list v-if="annotatedSpeakerQueue.length" density="comfortable" bg-color="background">
           <template v-for="{ title, queue } in speakerGroups" :key="title">
             <v-list-subheader v-if="title" class="mt-3">
               {{ title }}
