12bcca3d1b69ee214c701e6b8fc014154dc64d52
Johan Schiff <johan@radkompaniet.se>
2022-09-14 13:35:51 +0200

    automated build and docker push based on user feedback

--- build_dev.sh
+++ build_dev.sh
@@ -7,6 +7,17 @@ fi
 VERSION="$1"
 # BACKEND_VERSION="latest"
 # docker build . -t voteit/voteit4frontend:$VERSION --progress plain --build-arg BACKEND_VERSION=$BACKEND_VERSION
+
+read -p "Do you want to build distribution files now? [y/N] " -n 1 -r
+echo
+if [[ "$REPLY" =~ ^[yY]$ ]]; then
+  npm run build
+fi
+
 docker build . -t voteit/voteit4frontend:$VERSION --progress plain
-echo "If everything worked: "
-echo "docker push voteit/voteit4frontend:$VERSION"
+
+read -p "Do you want to push to Docker Hub? [y/N] " -n 1 -r
+echo
+if [[ "$REPLY" =~ ^[yY]$ ]]; then
+  docker push voteit/voteit4frontend:$VERSION
+fi
