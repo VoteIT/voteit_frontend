8d48043f61c03140f1770ebbb0b211dc2de577ac
Johan Schiff <johan@radkompaniet.se>
2022-10-21 17:03:18 +0200

    code cleanup

--- src/modules/proposals/AddProposalModal.vue
+++ src/modules/proposals/AddProposalModal.vue
@@ -45,159 +45,134 @@
   </div>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, PropType, ref, watch } from 'vue'
+<script lang="ts" setup>
+import { computed, PropType, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 import { stripHTML } from '@/utils'
 import { parseRestError } from '@/utils/restApi'
 
-import useAuthentication from '@/composables/useAuthentication'
-import useDefaults from '@/composables/useDefaults'
 import RichtextEditor from '@/components/RichtextEditor.vue'
-import { proposalType } from './contentTypes'
 import useAgenda from '../agendas/useAgenda'
 import useMeeting from '../meetings/useMeeting'
 import PostAs from '../meetings/PostAs.vue'
 import useMeetingGroups from '../meetings/useMeetingGroups'
-
-import type { PreviewProposal, Proposal } from './types'
 import type { Author } from '../meetings/types'
+
+import { proposalType } from './contentTypes'
+import type { PreviewProposal, Proposal } from './types'
 
 const previewDelay = 500 // Wait 1 s before preview
 let previewTimeout: number
 
-export default defineComponent({
-  emits: ['close'],
-  components: {
-    PostAs,
-    RichtextEditor
+defineEmits(['close'])
+const props = defineProps({
+  shortname: {
+    type: String as PropType<Proposal['shortname']>,
+    default: 'proposal'
   },
-  props: {
-    shortname: {
-      type: String as PropType<Proposal['shortname']>,
-      default: 'proposal'
-    },
-    extra: {
-      type: Object as PropType<Partial<Proposal>>,
-      default: () => ({})
-    },
-    modelValue: {
-      type: String,
-      default: ''
-    },
-    proposal: Object as PropType<Proposal>
+  extra: {
+    type: Object as PropType<Partial<Proposal>>,
+    default: () => ({})
   },
-  setup (props) {
-    const { t } = useI18n()
-    const body = ref(
-      props.proposal
-        ? props.proposal.body
-        : props.modelValue
-    )
-    const { meetingId } = useMeeting()
-    const { agendaId } = useAgenda(meetingId)
-    const { canPostAs } = useMeetingGroups(meetingId)
-    const { user } = useAuthentication()
+  modelValue: {
+    type: String,
+    default: ''
+  },
+  proposal: Object as PropType<Proposal>
+})
 
-    const author = ref<Partial<Author> | undefined>(
-      props.proposal && {
-        author: props.proposal.author,
-        meeting_group: props.proposal.meeting_group
-      } as Author
-    )
+const { t } = useI18n()
 
-    function getPatchData (): Partial<Proposal> {
-      return {
-        body: body.value,
-        ...(author.value || {})
-      }
-    }
+const body = ref(
+  props.proposal
+    ? props.proposal.body
+    : props.modelValue
+)
+const { meetingId } = useMeeting()
+const { agendaId } = useAgenda(meetingId)
+const { canPostAs } = useMeetingGroups(meetingId)
 
-    function getCreateData (): Partial<Proposal> {
-      return {
-        shortname: props.shortname,
-        agenda_item: agendaId.value,
-        ...props.extra,
-        ...getPatchData()
-      }
-    }
+const author = ref<Partial<Author> | undefined>(
+  props.proposal && {
+    author: props.proposal.author,
+    meeting_group: props.proposal.meeting_group
+  } as Author
+)
 
-    const api = proposalType.getContentApi({ alertOnError: false })
-    const proposalPreview = ref<Partial<Proposal> | null>(null)
-    const errors = ref<Record<string, string[]>>({})
-    const errorText = computed(() => {
-      const errs = errors.value.body ?? errors.value.__root__
-      if (!errs) return
-      return errs.join(', ')
-    })
-    async function preview () {
-      errors.value = {}
-      if (!body.value) return
-      try {
-        const { data } = await api.action<PreviewProposal>('preview', getCreateData())
-        proposalPreview.value = {
-          ...data,
-          created: new Date(),
-          pk: 0,
-          shortname: props.shortname
-        }
-      } catch (e) {
-        proposalPreview.value = null
-        errors.value = parseRestError(e)
-      }
-      previewing.value = false
-    }
-
-    const saving = ref(false)
-    const done = ref(false)
-    async function saveProposal () {
-      saving.value = true
-      try {
-        if (props.proposal) await proposalType.api.patch(props.proposal.pk, getPatchData())
-        else await proposalType.api.add(getCreateData())
-        done.value = true
-      } catch {}
-      saving.value = false
-    }
-
-    const previewing = ref(false)
-    function setPreviewTimeout () {
-      clearTimeout(previewTimeout)
-      if (!stripHTML(body.value)) {
-        previewing.value = false
-        proposalPreview.value = null
-        return
-      }
-      previewing.value = true
-      previewTimeout = setTimeout(
-        preview,
-        previewDelay
-      )
-    }
-
-    watch(author, preview)
-    watch(body, setPreviewTimeout)
-    watch(() => props.modelValue, value => { // React when used as subcomponent, i.e. in AddTextProposalModal
-      body.value = value
-    })
-
-    return {
-      t,
-      ...useDefaults(),
-      author,
-      canPostAs,
-      done,
-      errorText,
-      previewing,
-      proposalPreview,
-      body,
-      saving,
-      user,
-      saveProposal,
-      preview
-    }
+function getPatchData (): Partial<Proposal> {
+  return {
+    body: body.value,
+    ...(author.value || {})
   }
+}
+
+function getCreateData (): Partial<Proposal> {
+  return {
+    shortname: props.shortname,
+    agenda_item: agendaId.value,
+    ...props.extra,
+    ...getPatchData()
+  }
+}
+
+const api = proposalType.getContentApi({ alertOnError: false })
+const proposalPreview = ref<Partial<Proposal> | null>(null)
+const errors = ref<Record<string, string[]>>({})
+const errorText = computed(() => {
+  const errs = errors.value.body ?? errors.value.__root__
+  if (!errs) return
+  return errs.join(', ')
+})
+async function preview () {
+  errors.value = {}
+  if (!body.value) return
+  try {
+    const { data } = await api.action<PreviewProposal>('preview', getCreateData())
+    proposalPreview.value = {
+      ...data,
+      created: new Date(),
+      pk: 0,
+      shortname: props.shortname
+    }
+  } catch (e) {
+    proposalPreview.value = null
+    errors.value = parseRestError(e)
+  }
+  previewing.value = false
+}
+
+const saving = ref(false)
+const done = ref(false)
+async function saveProposal () {
+  saving.value = true
+  try {
+    if (props.proposal) await proposalType.api.patch(props.proposal.pk, getPatchData())
+    else await proposalType.api.add(getCreateData())
+    done.value = true
+  } catch {}
+  saving.value = false
+}
+
+const previewing = ref(false)
+function setPreviewTimeout () {
+  clearTimeout(previewTimeout)
+  if (!stripHTML(body.value)) {
+    previewing.value = false
+    proposalPreview.value = null
+    return
+  }
+  previewing.value = true
+  previewTimeout = setTimeout(
+    preview,
+    previewDelay
+  )
+}
+
+watch(author, preview)
+watch(body, setPreviewTimeout)
+watch(() => props.modelValue, value => { // React when used as subcomponent, i.e. in AddTextProposalModal
+  body.value = value
 })
 </script>
 
