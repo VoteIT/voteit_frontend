60935c08515bf3f119ab66508906efdd913204c8
Johan Schiff <johan@radkompaniet.se>
2022-11-21 13:21:10 +0100

    DurationInput arithmetic should use numbers, not strings, fixes #206

--- src/components/inputs/DurationInput.vue
+++ src/components/inputs/DurationInput.vue
@@ -1,34 +1,31 @@
 <template>
   <div class="d-flex">
-    <v-text-field type="number" min="0" label="Minutes" v-model="duration.minutes" />
+    <v-text-field type="number" min="0" :label="t('input.minutes')" v-model="duration.minutes" />
     <div class="mr-1" />
-    <v-text-field type="number" min="0" max="59" label="Seconds" v-model="duration.seconds" />
+    <v-text-field type="number" min="0" max="59" :label="t('input.seconds')" v-model="duration.seconds" />
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive, ref, watch } from 'vue'
+<script setup lang="ts">
+import { reactive, watch } from 'vue'
+import { useI18n } from 'vue-i18n'
 
-export default defineComponent({
-  props: {
-    modelValue: {
-      type: Number,
-      default: 0
-    }
-  },
-  setup (props, { emit }) {
-    const duration = reactive({
-      minutes: ref(Math.floor(props.modelValue / 60)),
-      seconds: ref(props.modelValue % 60)
-    })
-
-    watch(duration, ({ minutes, seconds }) => {
-      emit('update:modelValue', (minutes * 60) + seconds)
-    })
-
-    return {
-      duration
-    }
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    default: 0
   }
+})
+const emit = defineEmits(['update:modelValue'])
+
+const { t } = useI18n()
+
+const duration = reactive({
+  minutes: String(Math.floor(props.modelValue / 60)),
+  seconds: String(props.modelValue % 60)
+})
+
+watch(duration, ({ minutes, seconds }) => {
+  emit('update:modelValue', (Number(minutes) * 60) + Number(seconds))
 })
 </script>
