330d6ec2511a304bb42bbf6e86b55dc9313a20f3
Johan Schiff <johan@radkompaniet.se>
2022-12-02 15:42:49 +0100

    StartView as script setup

--- src/modules/meetings/StartView.vue
+++ src/modules/meetings/StartView.vue
@@ -14,8 +14,8 @@
   </v-row>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, reactive, ref, watch } from 'vue'
+<script lang="ts" setup>
+import { computed, reactive, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useTitle } from '@vueuse/core'
 
@@ -28,71 +28,52 @@ import { MenuItem } from '@/utils/types'
 import { meetingType } from './contentTypes'
 import useSpeakerSystems from '../speakerLists/useSpeakerSystems'
 import { MeetingState } from './types'
+import useDefaults from '@/composables/useDefaults'
 
-export default defineComponent({
-  inject: ['cols'],
-  components: {
-    Richtext,
-    WorkflowState,
-    Headline
-  },
-  setup () {
-    const { t } = useI18n()
-    const editing = ref(false)
-    const { meeting, meetingId, canChange } = useMeeting()
-    const { activeSpeakerSystems } = useSpeakerSystems(meetingId)
+const { t } = useI18n()
+const { cols } = useDefaults()
 
-    useTitle(computed(() => `${meeting.value?.title} | VoteIT`))
+const editing = ref(false)
+const { meeting, meetingId, canChange } = useMeeting()
+const { activeSpeakerSystems } = useSpeakerSystems(meetingId)
 
-    const content = reactive({
-      title: meeting.value?.title ?? '',
-      body: meeting.value?.body ?? ''
+useTitle(computed(() => `${meeting.value?.title} | VoteIT`))
+
+const content = reactive({
+  title: meeting.value?.title ?? '',
+  body: meeting.value?.body ?? ''
+})
+watch(meeting, value => {
+  if (editing.value) return
+  content.title = value?.title ?? ''
+  content.body = value?.body ?? ''
+})
+
+const menuItems = computed<MenuItem[]>(() => {
+  const items: MenuItem[] = []
+  if (canChange.value) {
+    items.push({
+      title: t('edit'),
+      prependIcon: 'mdi-pencil',
+      onClick: async () => { editing.value = true }
     })
-    watch(meeting, value => {
-      if (editing.value) return
-      content.title = value?.title ?? ''
-      content.body = value?.body ?? ''
-    })
-
-    const menuItems = computed<MenuItem[]>(() => {
-      const items: MenuItem[] = []
-      if (canChange.value) {
-        items.push({
-          title: t('edit'),
-          prependIcon: 'mdi-pencil',
-          onClick: async () => { editing.value = true }
-        })
-      }
-      if (activeSpeakerSystems.value.length) {
-        items.push('---')
-        for (const system of activeSpeakerSystems.value) {
-          items.push({
-            title: t('speaker.fullscreenSystem', { ...system }),
-            prependIcon: 'mdi-projector-screen-outline',
-            to: `/speakers/${meetingId.value}/${system.pk}`
-          })
-        }
-      }
-      return items
-    })
-
-    function submit () {
-      editing.value = false
-      if (content.title === meeting.value?.title && content.body === meeting.value?.body) return
-      meetingType.api.patch(meetingId.value, { ...content })
-    }
-
-    return {
-      canChange,
-      content,
-      MeetingState,
-      meetingType,
-      meeting,
-      menuItems,
-      editing,
-      submit,
-      t
+  }
+  if (activeSpeakerSystems.value.length) {
+    items.push('---')
+    for (const system of activeSpeakerSystems.value) {
+      items.push({
+        title: t('speaker.fullscreenSystem', { ...system }),
+        prependIcon: 'mdi-projector-screen-outline',
+        to: `/speakers/${meetingId.value}/${system.pk}`
+      })
     }
   }
+  return items
 })
+
+function submit () {
+  editing.value = false
+  if (content.title === meeting.value?.title && content.body === meeting.value?.body) return
+  meetingType.api.patch(meetingId.value, { ...content })
+}
 </script>
