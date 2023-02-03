da3cd61b7df0ae4726bee664e6bc6063eacf35ed
Johan Schiff <johan@radkompaniet.se>
2023-02-03 17:05:00 +0100

    display meeting dialect in control panel

---
+++ src/modules/meetings/dialects/QuickPanel.vue
@@ -0,0 +1,18 @@
+<template>
+  <template v-if="meeting?.dialect">
+    <v-card-text>
+      <strong>{{ meeting.dialect.title }}:</strong>
+      {{ meeting.dialect.description }}
+    </v-card-text>
+    <v-card-text>
+      <strong>Roller: </strong>
+      <v-chip v-for="{ role_id, title } in meeting.dialect.roles" :key="role_id" :text="title" />
+    </v-card-text>
+  </template>
+</template>
+
+<script lang="ts" setup>
+import useMeeting from '../useMeeting'
+
+const { meeting } = useMeeting()
+</script>
