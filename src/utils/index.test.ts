dac6646bdae1f899e2db34c6d8568e9477e364d3
Johan Schiff <johan@radkompaniet.se>
2023-03-13 22:17:30 +0100

    utils testing

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -1,6 +1,8 @@
-import { expect, test } from 'vitest'
+import moment from 'moment'
+import { expect, test, vi } from 'vitest'
 
-import { dateify, slugify, stripHTML, tagify, uriToPayload } from '.'
+import { dateify, dialogQuery, durationToString, sleep, slugify, stripHTML, tagify, uriToPayload } from '.'
+import { openDialogEvent } from './events'
 
 test('uriToPayload', () => {
   expect(uriToPayload('test/123')).toStrictEqual({ channel_type: 'test', pk: 123 })
@@ -34,4 +36,30 @@ test('dateify', () => {
 
 test('stripHTML', () => {
   expect(stripHTML('  <div>I\'m a <em>test pilot</em>.</div>  ')).toBe('I\'m a test pilot.')
+})
+
+test('dialogQuery', () => {
+  openDialogEvent.once(({ resolve }) => { resolve(true) })
+  expect(dialogQuery('Testing resolve')).resolves.toBe(true)
+
+  openDialogEvent.once(() => { throw new Error('error') })
+  expect(dialogQuery({ title: 'Testing reject' })).rejects.toBeInstanceOf(Error)
+})
+
+test('durationToString', () => {
+  // Moment js should be deprecated
+  expect(durationToString(moment.duration(299, 'minutes'))).toEqual('4:59:00')
+  expect(durationToString(moment.duration(123, 'minutes'))).toEqual('2:03:00')
+  expect(durationToString(moment.duration(59, 'minutes'))).toEqual('59:00')
+  expect(durationToString(moment.duration(60, 'seconds'))).toEqual('1:00')
+  expect(durationToString(moment.duration(59, 'seconds'))).toEqual('0:59')
+  expect(durationToString(moment.duration(1, 'second'))).toEqual('0:01')
+})
+
+test('sleep', async () => {
+  const start = performance.now()
+  await sleep(1)
+  const duration = performance.now() - start
+  expect(duration).toBeGreaterThan(1)
+  expect(duration).toBeLessThan(10) // This will never be very exact. As long as it's at least 1s and stops at some point, it's ok.
 })
