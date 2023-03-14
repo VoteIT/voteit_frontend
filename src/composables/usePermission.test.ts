d7ce3feaa3487e3c03d411c03e2a7a800cb9186f
Johan Schiff <johan@radkompaniet.se>
2023-03-14 12:49:01 +0100

    further testing

---
+++ src/composables/usePermission.test.ts
@@ -0,0 +1,39 @@
+import { openDialogEvent } from '@/utils/events'
+import { expect, test, vi } from 'vitest'
+import { nextTick, ref } from 'vue'
+
+import usePermission from './usePermission'
+
+vi.mock('vue-i18n', vi.fn(() => ({ useI18n: () => ({ t: (str: string) => str }) })))
+
+test('usePermission custom', async () => {
+  expect(usePermission).toBeTruthy()
+
+  const handler = vi.fn()
+  usePermission(ref(false), {}, handler)
+  expect(handler).toHaveBeenCalledOnce()
+
+  const permission = ref(true)
+  usePermission(permission, {}, handler)
+  expect(handler).toHaveBeenCalledOnce()
+
+  permission.value = false
+  await nextTick() // Won't be called until next tick
+  expect(handler).toHaveBeenCalledTimes(2)
+})
+
+test('usePermission default', async () => {
+  const fn = vi.fn()
+  openDialogEvent.on(fn)
+
+  usePermission(ref(false))
+  expect(fn).toHaveBeenCalledOnce()
+  expect(fn).toHaveBeenLastCalledWith(
+    expect.objectContaining({
+      dismissible: false,
+      theme: 'error',
+      title: 'permission.defaultMessage',
+      yes: 'ok'
+    }
+  ))
+})
