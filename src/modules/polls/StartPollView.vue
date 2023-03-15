de14282670be268f219b8c1706c354766f6effa9
Johan Schiff <johan@radkompaniet.se>
2023-03-15 22:22:03 +0100

    rollback createPoll mistake

--- src/modules/polls/StartPollView.vue
+++ src/modules/polls/StartPollView.vue
@@ -80,7 +80,7 @@
         </v-expansion-panels>
         <div class="btn-group mt-3">
           <v-btn prepend-icon="mdi-undo-variant" @click="pickMethod = false" color="primary">{{ t('navigation.back') }}</v-btn>
-          <v-btn prepend-icon="mdi-check" color="primary" :disabled="!readyToCreate" @click="createPoll">{{ t('create') }}</v-btn>
+          <v-btn prepend-icon="mdi-check" color="primary" :disabled="!readyToCreate" @click="createPoll()">{{ t('create') }}</v-btn>
           <v-btn v-if="agendaItem?.state === 'ongoing'" prepend-icon="mdi-play" color="primary" :disabled="!readyToCreate" @click="createPoll(true)">{{ t('poll.createAndStart') }}</v-btn>
         </div>
       </template>
