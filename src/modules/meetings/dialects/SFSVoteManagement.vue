f55b5867c13562de73c41a2ee3ee4ce3dd9a36cc
Robin Harms Oredsson <robin@betahaus.net>
2023-04-05 17:05:09 +0200

    Probably accidental test leftover. Closes #272

--- src/modules/meetings/dialects/SFSVoteManagement.vue
+++ src/modules/meetings/dialects/SFSVoteManagement.vue
@@ -86,7 +86,7 @@ const canAssignVotes = computed(() => {
   return (
     !isFinishedMeeting(meetingId.value) &&
     (
-      !isModerator.value ||
+      isModerator.value ||
       props.group.memberships.some(member => member.user === user.value?.pk && member.role === leaderRoleId.value)
     )
   )
