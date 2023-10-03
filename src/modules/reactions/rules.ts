e869c32ecd4486b9a117cfc86e4039a12f020378
Johan Schiff <johan@betahaus.net>
2023-10-03 16:42:12 +0200

    reactions only in active meetings (upcoming, ongoing)

--- src/modules/reactions/rules.ts
+++ src/modules/reactions/rules.ts
@@ -3,14 +3,16 @@ import { MeetingRole } from '../meetings/types'
 
 import { reactionButtons } from './useReactions'
 import { Reaction, ReactionButton, isFlagButton } from './types'
+import { isActiveMeeting } from '../meetings/rules'
 
 const { hasRole } = meetingType.useContextRoles()
 
 export function canAddReaction (button: ReactionButton): boolean {
+  if (!isActiveMeeting(button.meeting) || !button.active) return false
   const required = isFlagButton(button)
     ? MeetingRole.Moderator
     : button.change_roles
-  return button.active && !!hasRole(button.meeting, required)
+  return !!hasRole(button.meeting, required)
 }
 
 export function canDeleteReaction (reaction: Reaction): boolean {
