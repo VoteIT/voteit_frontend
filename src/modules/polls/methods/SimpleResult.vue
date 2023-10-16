c8ec77f64f6271b6e0658daff1d1e9b36cf87b55
Johan Schiff <johan@betahaus.net>
2023-10-16 14:49:19 +0200

    [prettier]

--- src/modules/polls/methods/SimpleResult.vue
+++ src/modules/polls/methods/SimpleResult.vue
@@ -9,11 +9,13 @@
     >
       <template #bottom-right>
         <div class="mt-2 flex-grow-1 text-right">
-          <v-btn v-for="{ btn, key, percentage, value } in choices" :key="key" v-bind="btn">
+          <v-btn
+            v-for="{ btn, key, percentage, value } in choices"
+            :key="key"
+            v-bind="btn"
+          >
             {{ value }}
-            <small v-if="percentage" class="ml-1">
-              ({{ percentage }} %)
-            </small>
+            <small v-if="percentage" class="ml-1"> ({{ percentage }} %) </small>
           </v-btn>
         </div>
       </template>
@@ -54,45 +56,44 @@ const props = defineProps<{
 
 const { getProposal } = useProposals()
 
-function isProposal (prop?: P): prop is P {
+function isProposal(prop?: P): prop is P {
   return !!prop
 }
 
-function getActiveChoice (pk: number): SimpleChoice | undefined {
+function getActiveChoice(pk: number): SimpleChoice | undefined {
   if (props.result.approved.includes(pk)) return SimpleChoice.Yes
   if (props.result.denied.includes(pk)) return SimpleChoice.No
 }
 
-function transformResult (proposal: P): ProposalResult {
+function transformResult(proposal: P): ProposalResult {
   const pk = proposal.pk
   const result = props.result.results[pk]
   const total = result[SimpleChoice.Yes] + result[SimpleChoice.No]
 
   return {
     proposal,
-    choices: simpleChoices
-      .map(c => {
-        const activeChoice = getActiveChoice(pk)
-        // Add poll abstain_count to abstains
-        const value = c.value === SimpleChoice.Abstain
+    choices: simpleChoices.map((c) => {
+      const activeChoice = getActiveChoice(pk)
+      // Add poll abstain_count to abstains
+      const value =
+        c.value === SimpleChoice.Abstain
           ? result[c.value] + props.abstainCount
           : result[c.value]
-        const percentage = c.value === SimpleChoice.Abstain
+      const percentage =
+        c.value === SimpleChoice.Abstain
           ? undefined
-          : Math.round(value / total * 100)
-        return {
-          key: c.value,
-          percentage,
-          value,
-          btn: {
-            variant: activeChoice === c.value
-              ? 'elevated'
-              : 'text',
-            color: c.color,
-            prependIcon: c.icon
-          }
+          : Math.round((value / total) * 100)
+      return {
+        key: c.value,
+        percentage,
+        value,
+        btn: {
+          variant: activeChoice === c.value ? 'elevated' : 'text',
+          color: c.color,
+          prependIcon: c.icon
         }
-      })
+      }
+    })
   }
 }
 
