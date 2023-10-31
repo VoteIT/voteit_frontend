75385ea8b61e83371d0a091fb4c64968a6d2dfd3
Johan Schiff <johan@betahaus.net>
2023-10-31 20:32:28 +0100

    add transitions to proposals and comments

--- src/modules/proposals/AgendaProposals.vue
+++ src/modules/proposals/AgendaProposals.vue
@@ -1,9 +1,11 @@
 <template>
-  <Proposal v-for="p in proposals" :key="p.pk" :p="p" class="mb-4">
-    <template #buttons>
-      <ButtonPlugins :proposal="p" />
-    </template>
-  </Proposal>
+  <v-slide-x-transition group>
+    <Proposal v-for="p in proposals" :key="p.pk" :p="p" class="mb-4">
+      <template #buttons>
+        <ButtonPlugins :proposal="p" />
+      </template>
+    </Proposal>
+  </v-slide-x-transition>
 </template>
 
 <script lang="ts" setup>
