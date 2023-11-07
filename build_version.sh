fb207b6a572a23fb5fd6c392b19de949fd726d18
Johan Schiff <johan@betahaus.net>
2023-11-07 13:30:55 +0100

    when building new version, always build new dist files w version number

--- build_version.sh
+++ build_version.sh
@@ -8,19 +8,17 @@ VERSION="$1"
 # BACKEND_VERSION="latest"
 # docker build . -t voteit/voteit4frontend:$VERSION --progress plain --build-arg BACKEND_VERSION=$BACKEND_VERSION
 
-read -p "Do you want to build distribution files now? [y/N] " -n 1 -r
-echo
-if [[ "$REPLY" =~ ^[yY]$ ]]; then
-  # Ugly
-  # If .env.production is ever needed, do this in another way!
-  if [ -e '.env.production' ]; then
-    echo 'File .env.production must not exist'
-    exit 1
-  fi
-  echo VITE_FRONTEND_VERSION=$VERSION > .env.production
-  npm run build
-  rm .env.production
+# Ugly
+# If .env.production is ever needed, do this in another way!
+if [ -e '.env.production' ]; then
+  echo 'File .env.production must not exist'
+  exit 1
 fi
+echo VITE_FRONTEND_VERSION=$VERSION > .env.production
+# Always build dist files
+npm run build
+# Clean up temp file
+rm .env.production
 
 docker pull nginx:1.23-alpine
 docker build . -t voteit/voteit4frontend:$VERSION --platform linux/amd64
