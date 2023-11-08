83c8f9afa1b5671fd135d14caa85ecd1474f2873
Johan Schiff <johan@betahaus.net>
2023-11-08 21:14:10 +0100

    fix double participant role

--- src/modules/meetings/constants.ts
+++ src/modules/meetings/constants.ts
@@ -1,9 +1,8 @@
-import { MeetingRole } from './types';
+import { MeetingRole } from './types'
 
 export const DEFAULT_ROLE_ORDER = [
   MeetingRole.Participant,
   MeetingRole.Moderator,
-  MeetingRole.Participant,
   MeetingRole.PotentialVoter,
   MeetingRole.Proposer,
   MeetingRole.Discusser
