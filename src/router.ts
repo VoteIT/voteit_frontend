e43ab0817a43db408542b69dfd36b83cd69bb2c5
Johan Schiff <johan@betahaus.net>
2023-11-09 18:35:57 +0100

    remove unused speaker fullscreen view

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PageNotFoundView from '@/views/PageNotFoundView.vue'
 import appBar from '@/components/AppBar.vue'
 
-// import speakerFullscreen from '@/modules/speakerLists/fullscreenRouter'
 import plenary from '@/modules/plenary/router'
 import meeting from '@/modules/meetings/router'
 
@@ -17,7 +16,6 @@ const routes = [
   },
   ...meeting,
   plenary,
-  // speakerFullscreen,
   // Other pages, do a 404
   {
     path: '/:pathMatch(.*)*',
