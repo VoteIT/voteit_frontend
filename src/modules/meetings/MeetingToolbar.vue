38fcfa7e3e174ab8b27aabf37829fda8edd70e40
Johan Schiff <johan@betahaus.net>
2023-11-14 13:43:14 +0100

    fix uncommon problem, where teleport would fail before components are mounted

--- src/modules/meetings/MeetingToolbar.vue
+++ src/modules/meetings/MeetingToolbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <teleport to="#toolbar">
+  <teleport v-if="mounted" to="#toolbar">
     <v-toolbar
       color="secondary-lighten-2"
       elevation="1"
@@ -15,9 +15,18 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import { ref } from 'vue'
+
 interface Props {
   title?: string
 }
 
 defineProps<Props>()
+
+// This is a workaround to solve an issue with teleport,
+// where parent components have not rendered, and
+// #toolbar element is not present.
+const mounted = ref(false)
+onMounted(() => (mounted.value = true))
 </script>
