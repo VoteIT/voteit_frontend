4f9920400372c2adcf8807af2c662236902b62ca
Johan Schiff <johan@betahaus.net>
2023-11-17 18:41:14 +0100

    schulze result display winning proposal

--- src/modules/polls/methods/SchulzeResult.vue
+++ src/modules/polls/methods/SchulzeResult.vue
@@ -8,26 +8,78 @@
       {{ t('poll.schulze.numTiedWinners', tiedWinners.length) }}
     </p>
     <p v-if="tiedWinners.length" class="my-2">
-      <Tag v-for="tag, i in tiedWinners" :key="i" class="mr-1" :name="tag" disabled />
+      <Tag
+        v-for="(tag, i) in tiedWinners"
+        :key="i"
+        class="mr-1"
+        :name="tag"
+        disabled
+      />
     </p>
+    <template v-if="denyProposalWon">
+      <h3 v-if="denyProposalWon || winningProposal" class="mt-8 mb-1">
+        {{ t('poll.winningProposal') }}
+      </h3>
+      <v-card
+        v-if="denyProposalWon"
+        color="warning"
+        :title="t('poll.deny')"
+        class="mb-8"
+      />
+    </template>
+    <Proposal
+      v-else-if="winningProposal"
+      :p="winningProposal"
+      readOnly
+      class="my-8"
+    >
+      <template #bottom-right>
+        <v-btn color="success" prepend-icon="mdi-thumb-up">
+          {{ t('poll.winningProposal') }}
+        </v-btn>
+      </template>
+    </Proposal>
     <v-expansion-panels multiple class="my-4">
-      <v-expansion-panel v-for="{ btn, proposal, pairs } in proposalPairs" :key="proposal?.pk ?? 0">
+      <v-expansion-panel
+        v-for="{ btn, proposal, pairs } in proposalPairs"
+        :key="proposal?.pk ?? 0"
+      >
         <v-expansion-panel-title>
           <v-icon v-bind="btn" class="mr-4" />
           <Tag v-if="proposal" disabled :name="proposal.prop_id" />
-          <span v-else class="rounded bg-warning px-2 py-1">{{ t('poll.deny') }}</span>
+          <span v-else class="rounded bg-warning px-2 py-1">{{
+            t('poll.deny')
+          }}</span>
         </v-expansion-panel-title>
-        <v-expansion-panel-text v-for="pair in pairs" :key="`${proposal?.pk ?? 0} vs ${pair.proposal?.pk ?? 0}`" class="my-2">
+        <v-expansion-panel-text
+          v-for="pair in pairs"
+          :key="`${proposal?.pk ?? 0} vs ${pair.proposal?.pk ?? 0}`"
+          class="my-2"
+        >
           {{ t('poll.result.versus') }}:
-            <Tag v-if="pair.proposal" disabled :name="pair.proposal.prop_id" />
-            <span v-else class="rounded bg-warning px-2">{{ t('poll.deny') }}</span>
+          <Tag v-if="pair.proposal" disabled :name="pair.proposal.prop_id" />
+          <span v-else class="rounded bg-warning px-2">{{
+            t('poll.deny')
+          }}</span>
           <div class="d-flex justify-space-between mt-2">
-            <span class="bg-success px-2 rounded-pill">{{ t('poll.result.approveThis') }} ({{ pair.approve }})</span>
-            <span class="bg-secondary px-2 rounded-pill">{{ t('poll.result.tie') }} ({{ pair.tie }})</span>
-            <span class="bg-warning px-2 rounded-pill">{{ t('poll.result.approveOther') }} ({{ pair.deny }})</span>
+            <span class="bg-success px-2 rounded-pill"
+              >{{ t('poll.result.approveThis') }} ({{ pair.approve }})</span
+            >
+            <span class="bg-secondary px-2 rounded-pill"
+              >{{ t('poll.result.tie') }} ({{ pair.tie }})</span
+            >
+            <span class="bg-warning px-2 rounded-pill"
+              >{{ t('poll.result.approveOther') }} ({{ pair.deny }})</span
+            >
           </div>
           <div class="d-flex mt-1 overflow-hidden rounded">
-            <div v-for="({ percentage, color }, i) in pair.results" :key="`${proposal?.pk ?? 0} vs ${pair.proposal?.pk ?? 0} ${i}`" :class="`bg-${color}`" class="text-center overflow-hidden text-no-wrap" :style="{ width: `${percentage}%` }">
+            <div
+              v-for="({ percentage, color }, i) in pair.results"
+              :key="`${proposal?.pk ?? 0} vs ${pair.proposal?.pk ?? 0} ${i}`"
+              :class="`bg-${color}`"
+              class="text-center overflow-hidden text-no-wrap"
+              :style="{ width: `${percentage}%` }"
+            >
               {{ Math.round(percentage) }} %
             </div>
           </div>
@@ -46,6 +98,7 @@ import useProposals from '@/modules/proposals/useProposals'
 
 import { ThemeColor } from '@/utils/types'
 import { SchulzeResult } from './types'
+import { orderBy } from 'lodash'
 
 const props = defineProps<{
   abstainCount: number
@@ -58,11 +111,9 @@ const { getProposal } = useProposals()
 
 const tiedWinners = computed(() => {
   if (!props.result.tied_winners) return []
-  return props.result.tied_winners.map(pk => {
+  return props.result.tied_winners.map((pk) => {
     const prop = getProposal(pk)
-    return prop
-      ? prop.prop_id
-      : t('proposal.unknown')
+    return prop ? prop.prop_id : t('proposal.unknown')
   })
 })
 
@@ -84,8 +135,8 @@ const sortedProposals = computed(() => {
 
 const proposalCombinations = computed(() => {
   // All possible combinations of proposals
-  return sortedProposals.value.flatMap(
-    (one, i) => sortedProposals.value.slice(i + 1).map(other => [one, other])
+  return sortedProposals.value.flatMap((one, i) =>
+    sortedProposals.value.slice(i + 1).map((other) => [one, other])
   )
 })
 
@@ -94,49 +145,68 @@ const voteCount = computed(() => {
   // (To handle historic repeated schulze rounds)
   return props.result.vote_count
     ? props.result.vote_count
-    : Math.max(...proposalCombinations.value.map(([one, other]) => strengthMap.value[one][other] + strengthMap.value[other][one]))
+    : Math.max(
+        ...proposalCombinations.value.map(
+          ([one, other]) =>
+            strengthMap.value[one][other] + strengthMap.value[other][one]
+        )
+      )
 })
 
-function mapProposal (proposals: number[]) {
-  return proposals.map(pk => {
-    return {
-      proposal: getProposal(pk),
-      btn: props.result.winner === pk
-        ? { icon: 'mdi-thumb-up', color: ThemeColor.Success }
-        : { icon: 'mdi-thumb-down', color: ThemeColor.Warning },
-      pairs: props.result.candidates
-        .filter(n => n !== pk)
-        .map(other => {
-          const myStrength = strengthMap.value[pk][other]
-          const otherStrength = strengthMap.value[other][pk]
-          const tiedStrength = voteCount.value - myStrength - otherStrength
-          return {
-            proposal: getProposal(other),
-            approve: myStrength,
-            tie: tiedStrength,
-            deny: otherStrength,
-            // Percentages
-            results: [
-              {
-                percentage: myStrength / voteCount.value * 100,
-                color: ThemeColor.Success
-              },
-              {
-                percentage: tiedStrength / voteCount.value * 100,
-                color: ThemeColor.Secondary
-              },
-              {
-                percentage: otherStrength / voteCount.value * 100,
-                color: ThemeColor.Warning
-              }
-            ]
-          }
-        })
-    }
-  })
+function mapProposal(proposals: number[]) {
+  return orderBy(
+    proposals.map((pk) => {
+      const winner = props.result.winner === pk
+      return {
+        proposal: getProposal(pk),
+        winner,
+        btn: winner
+          ? { icon: 'mdi-thumb-up', color: ThemeColor.Success }
+          : { icon: 'mdi-thumb-down', color: ThemeColor.Warning },
+        pairs: props.result.candidates
+          .filter((n) => n !== pk)
+          .map((other) => {
+            const myStrength = strengthMap.value[pk][other]
+            const otherStrength = strengthMap.value[other][pk]
+            const tiedStrength = voteCount.value - myStrength - otherStrength
+            return {
+              proposal: getProposal(other),
+              approve: myStrength,
+              tie: tiedStrength,
+              deny: otherStrength,
+              // Percentages
+              results: [
+                {
+                  percentage: (myStrength / voteCount.value) * 100,
+                  color: ThemeColor.Success
+                },
+                {
+                  percentage: (tiedStrength / voteCount.value) * 100,
+                  color: ThemeColor.Secondary
+                },
+                {
+                  percentage: (otherStrength / voteCount.value) * 100,
+                  color: ThemeColor.Warning
+                }
+              ]
+            }
+          })
+      }
+    }),
+    'winner',
+    'desc'
+  )
 }
 
 const proposalPairs = computed(() => {
   return mapProposal(sortedProposals.value)
+})
+
+const winningProposal = computed(() => {
+  if (!props.result.winner) return
+  return getProposal(props.result.winner)
+})
+const denyProposalWon = computed(() => {
+  return props.result.winner === 0
 })
 </script>
