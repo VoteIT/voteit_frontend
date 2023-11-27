9f544c2ff0562b96aa63e50916d4baf157d9f47c
Johan Schiff <johan@betahaus.net>
2023-11-27 08:58:56 +0100

    Moment - display exact time on hover

--- src/components/Moment.vue
+++ src/components/Moment.vue
@@ -1,9 +1,18 @@
 <template>
-  <span :class="{ time: !ordinary }">{{ prepend }} {{ fromNow }}</span>
+  <span v-if="inSeconds" :class="{ time: !ordinary }">
+    {{ prepend }} {{ fromNow }}
+  </span>
+  <v-tooltip v-else :text="exactDate">
+    <template #activator="{ props }">
+      <span v-bind="{ ...props, ...$attrs }" :class="{ time: !ordinary }">
+        {{ prepend }} {{ fromNow }}
+      </span>
+    </template>
+  </v-tooltip>
 </template>
 
 <script setup lang="ts">
-import { Duration } from 'luxon'
+import { Duration, DateTime } from 'luxon'
 import { computed, nextTick, ref, watch, watchEffect } from 'vue'
 import { useIntervalFn } from '@vueuse/shared'
 
@@ -23,20 +32,30 @@ const props = defineProps<{
 const { dateTime, serverNow } = useServerDateTime(computed(() => props.date))
 const fromNow = ref('')
 
-function updateFromNow () {
+const exactDate = computed(() =>
+  DateTime.fromISO(props.date).toLocaleString({
+    dateStyle: 'long',
+    timeStyle: 'long'
+  })
+)
+
+function updateFromNow() {
   // Can not be computed(), because time is not reactive
   const serverDateTime = serverNow()
   const serverDiff = serverDateTime.diff(dateTime.value)
   if (props.inSeconds) {
     fromNow.value = durationToString(serverDiff)
   } else {
-    fromNow.value = serverDiff > ABSOLUTE_BREAKPOINT
-      ? dateTime.value.toLocaleString()
-      : dateTime.value.toRelative({ base: serverDateTime }) || ''
+    fromNow.value =
+      serverDiff > ABSOLUTE_BREAKPOINT
+        ? dateTime.value.toLocaleString()
+        : dateTime.value.toRelative({ base: serverDateTime }) || ''
   }
 }
 
-useIntervalFn(updateFromNow, props.inSeconds ? 1_000 : 60_000, { immediateCallback: true })
+useIntervalFn(updateFromNow, props.inSeconds ? 1_000 : 60_000, {
+  immediateCallback: true
+})
 watchEffect(updateFromNow)
 watch(currentLocale, () => nextTick(updateFromNow))
 </script>
