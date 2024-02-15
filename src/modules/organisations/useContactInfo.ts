bc637914c8d884649e13a11c7db94cb2aa3a7ac7
Johan Schiff <johan@betahaus.net>
2024-02-15 14:32:33 +0100

    missing file

---
+++ src/modules/organisations/useContactInfo.ts
@@ -0,0 +1,60 @@
+import { SocketState, socket, socketState } from '@/utils/Socket'
+import { computed, ref, watch } from 'vue'
+import useOrganisation from './useOrganisation'
+
+export interface ContactInfo {
+  text: string
+  generic_email: string
+  invoice_email: string
+  invoice_info: string
+  modified: string // ISO date
+  organisation: number
+  pk: number
+  requires_check: boolean
+}
+
+const { canChangeOrganisation } = useOrganisation()
+const contactInfo = ref<ContactInfo | null>(null)
+const requiresCheck = computed(() => contactInfo.value?.requires_check)
+
+async function fetchContactInfo() {
+  const { p } = await socket.call<ContactInfo>('contact_info.get')
+  contactInfo.value = p
+  return p
+}
+
+async function saveContactInfo(data: ContactInfo) {
+  const { p } = await socket.call<ContactInfo>('contact_info.set', data)
+  contactInfo.value = p
+  return p
+}
+
+/**
+ * @param quietCheck If true, contact info will be fetched if and when user has permission to change organisation. Errors are suppressed.
+ * @returns
+ */
+export default function useContactInfo(quietCheck = false) {
+  // Trigger when socket is open, user is manager and quietCheck is set
+  watch(
+    () =>
+      canChangeOrganisation.value &&
+      socketState.value === SocketState.Open &&
+      quietCheck,
+    async (value) => {
+      if (!value) return
+      try {
+        fetchContactInfo()
+      } catch {
+        // Suppress error
+      }
+    },
+    { immediate: true }
+  )
+
+  return {
+    contactInfo,
+    requiresCheck,
+    fetchContactInfo,
+    saveContactInfo
+  }
+}
