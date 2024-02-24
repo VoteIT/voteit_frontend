29d19d1762e22e4bd6a54c68d71843d3cc9f5424
Johan Schiff <johan@betahaus.net>
2024-02-24 08:37:23 +0100

    Reusable button w dropdown component

---
+++ src/components/ButtonWithDropdown.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+defineProps<{
+  color?: string
+  size?: 'small'
+  text: string
+}>()
+</script>
+
+<template>
+  <span>
+    <v-btn
+      :color="color"
+      :size="size"
+      :text="text"
+      class="rounded-e-0 pr-2"
+      variant="flat"
+      v-bind="$attrs"
+    />
+    <v-menu>
+      <template #activator="{ props }">
+        <v-btn
+          :color="color"
+          :size="size"
+          variant="flat"
+          v-bind="props"
+          class="rounded-s-0 px-2"
+        >
+          <v-icon>mdi-chevron-down</v-icon>
+        </v-btn>
+      </template>
+      <slot></slot>
+    </v-menu>
+  </span>
+</template>
