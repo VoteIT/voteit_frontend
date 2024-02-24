d0cc449cf7062b01a8ff2f6c8ac8c2e10b2d33cc
Johan Schiff <johan@betahaus.net>
2024-02-24 20:53:46 +0100

    New AuthorAvator component

---
+++ src/modules/meetings/AuthorAvatar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import UserAvatar from '@/components/UserAvatar.vue'
+import { Author, isGroupAuthor } from './types'
+
+defineProps<{
+  author: Author
+  size?: 'small'
+}>()
+</script>
+
+<template>
+  <v-avatar
+    v-if="isGroupAuthor(author)"
+    color="secondary"
+    icon="mdi-account-multiple"
+    :size="size"
+  />
+  <UserAvatar v-else popup :pk="author.author" :size="size" />
+</template>
