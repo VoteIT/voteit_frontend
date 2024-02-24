151231632c8f414acbc7a6aac0a6be01da15139a
Johan Schiff <johan@betahaus.net>
2024-02-24 20:04:51 +0100

    Complete rewrite of PostAs component, closes #350

--- src/modules/meetings/PostAs.vue
+++ src/modules/meetings/PostAs.vue
@@ -1,175 +1,206 @@
-<template>
-  <v-autocomplete
-    v-if="options"
-    :label="t('proposal.postAs')"
-    :items="options"
-    v-model="postAs"
-    v-model:search="search"
-    :no-data-text="t('noSuggestions')"
-    hide-details
-    density="compact"
-  />
-</template>
-
-<script lang="ts">
+<script lang="ts" setup>
 /* eslint-disable camelcase */
-import {
-  computed,
-  defineComponent,
-  PropType,
-  reactive,
-  Ref,
-  ref,
-  watch
-} from 'vue'
+import { computed, Ref, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 
+import { getFullName } from '@/utils'
 import useAuthentication from '@/composables/useAuthentication'
+
 import type { IUser } from '../organisations/types'
 import useUserDetails from '../organisations/useUserDetails'
+import { userType } from '../organisations/contentTypes'
 
 import useMeeting from './useMeeting'
 import useMeetingGroups from './useMeetingGroups'
-import { userType } from '../organisations/contentTypes'
-import { Author } from './types'
-import { getFullName } from '@/utils'
+import { Author, isGroupAuthor, isUserAuthor } from './types'
 
 interface AutocompleteItem {
   value: string
   title: string
 }
 
-// Remember post as selection by meeting id
-const postAsDefaults = reactive(new Map<number, Partial<Author>>())
+const emit = defineEmits<{ (e: 'update:modelValue', value: Author): void }>()
+const props = defineProps<{
+  modelValue?: Author
+}>()
 
-export default defineComponent({
-  emits: ['update:modelValue'],
-  props: {
-    modelValue: Object as PropType<Author>
+const { t } = useI18n()
+const { user } = useAuthentication()
+const { isModerator, meetingId, postAs } = useMeeting()
+const { meetingGroups, postAsGroups, getMeetingGroup } =
+  useMeetingGroups(meetingId)
+const { getUser } = useUserDetails()
+
+// This means we're editing pre-existing content
+const editingMode = !!props.modelValue
+
+const author: Ref<Author> = editingMode ? ref(props.modelValue) : postAs
+
+watch(
+  author,
+  (author) => {
+    emit('update:modelValue', author)
   },
-  setup(props, { emit }) {
-    const { t } = useI18n()
-    const { user } = useAuthentication()
-    const { meetingId, isModerator } = useMeeting()
-    const { meetingGroups, userGroups } = useMeetingGroups(meetingId)
-    const { getUser } = useUserDetails()
+  { immediate: true }
+)
 
-    // This means we're editing existing content
-    const editingMode = !!props.modelValue
+/**
+ * Convert API author value to a string used as value in select
+ */
+function authorToString(author: Author): string {
+  return isGroupAuthor(author)
+    ? `g:${author.author}:${author.meeting_group}`
+    : isUserAuthor(author)
+    ? `u:${author.author}`
+    : `wg:${author.author}:${author.meeting_group}`
+}
 
-    const value: Ref<Partial<Author>> = editingMode
-      ? ref(props.modelValue)
-      : computed({
-          get() {
-            return (
-              postAsDefaults.get(meetingId.value) || {
-                author: user.value?.pk
-              }
-            )
-          },
-          set(author) {
-            if (author) postAsDefaults.set(meetingId.value, author)
-          }
-        })
-
-    watch(
-      value,
-      (author) => {
-        emit('update:modelValue', author)
-      },
-      { immediate: true }
-    )
-
-    function userToAutocomplete(user: IUser): AutocompleteItem {
+/**
+ * Convert string value from select to API author value
+ */
+function stringToAuthor(value: string): Author {
+  const [type, usr, grp] = value.split(':')
+  switch (type) {
+    case 'g':
       return {
-        title: `${getFullName(user)} (${user.userid})`,
-        value: `user:${user.pk}`
+        as_group: true,
+        author: Number(usr),
+        meeting_group: Number(grp)
       }
-    }
-    const postAs = computed<string | undefined>({
-      get() {
-        if (!value.value) return
-        const { meeting_group, author } = value.value
-        return meeting_group ? `group:${meeting_group}` : `user:${author}`
-      },
-      set(newValue) {
-        if (!newValue) return
-        const [type, pk] = newValue.split(':')
-        value.value =
-          type === 'group'
-            ? { meeting_group: Number(pk) }
-            : { author: Number(pk), meeting_group: null }
+    case 'wg':
+      return {
+        as_group: false,
+        author: Number(usr),
+        meeting_group: Number(grp)
       }
-    })
-    const search = ref('')
-    function getInitialUserOption() {
-      const author = value.value.author
-      if (author && author !== user.value?.pk) {
-        const user = getUser(author)
-        if (user) return [userToAutocomplete(user)]
+    case 'u':
+      return {
+        as_group: false,
+        author: Number(usr),
+        meeting_group: null
       }
-      return []
-    }
-    const userOptions = ref<AutocompleteItem[]>(getInitialUserOption())
+    default:
+      throw new Error(`Unexpected type '${type}'`)
+  }
+}
 
-    function preserveCurrentAuthor(
-      newOptions?: AutocompleteItem[]
-    ): AutocompleteItem[] {
-      newOptions = newOptions ?? []
-      const { author } = value.value
-      if (!author) return newOptions
-      const finder = (item: AutocompleteItem) =>
-        author === Number(item.value.split(':')[1])
-      // Ok, it's in the new options as well
-      if (newOptions.find(finder)) return newOptions
-      // Not in new options - preserve this
-      const preserve = userOptions.value.find(finder)
-      // Should always find this, but let's not take anything for granted
-      return preserve ? [preserve, ...newOptions] : newOptions
-    }
+/**
+ * Convert Author to AutocompleteItem, used in select.
+ */
+function authorToAutocompleteItem(author: Author): AutocompleteItem {
+  const value = authorToString(author)
+  const user = getUser(author.author)
+  const fullName = user ? getFullName(user) : '-'
+  return {
+    title: isGroupAuthor(author)
+      ? getMeetingGroup(author.meeting_group)?.title ?? '-'
+      : isUserAuthor(author)
+      ? `${fullName} (${user?.userid ?? '-'})`
+      : `${fullName} (${getMeetingGroup(author.meeting_group)?.title ?? '-'})`,
+    value
+  }
+}
 
-    watch(search, async (search: string) => {
-      // Only moderators can switch user
-      if (!isModerator.value) return
-      if (!search.length) {
-        userOptions.value = preserveCurrentAuthor()
-        return
-      }
-      const { data } = await userType.api.list({
-        meeting: meetingId.value,
-        search
-      })
-      const newOptions = data
-        .filter(({ pk }) => user.value?.pk !== pk)
-        .map(userToAutocomplete)
-      userOptions.value = preserveCurrentAuthor(newOptions)
+function* userToAutocomplete(user: IUser): Generator<AutocompleteItem> {
+  const base = {
+    as_group: false,
+    author: user.pk,
+    meeting_group: null
+  } as const
+  yield authorToAutocompleteItem(base)
+  for (const g of meetingGroups.value) {
+    if (!g.members.includes(user.pk)) continue
+    yield authorToAutocompleteItem({
+      ...base,
+      meeting_group: g.pk
     })
-    const options = computed(() => {
-      const groups = isModerator.value ? meetingGroups : userGroups
-      if (!groups.value.length) return
-      return [
-        user.value
-          ? userToAutocomplete(user.value)
-          : {
-              // Should not happen
-              value: null,
-              title: 'self'
-            },
-        ...groups.value.map(({ pk, title }) => ({
-          value: `group:${pk}`,
-          title
-        })),
-        ...userOptions.value
-      ]
-    })
+  }
+}
 
-    return {
-      t,
-      postAs,
-      options,
-      search,
-      userOptions
-    }
+function* usersToAutocomplete(users: IUser[]): Generator<AutocompleteItem> {
+  for (const u of users) {
+    for (const v of userToAutocomplete(u)) yield v
+  }
+}
+
+const postAsSelect = computed<string | undefined>({
+  get() {
+    return authorToString(author.value)
+  },
+  set(newValue) {
+    if (!newValue) return
+    author.value = stringToAuthor(newValue)
   }
 })
+
+/**
+ * Search options logic
+ */
+const searchOptions = ref<AutocompleteItem[]>([])
+const search = ref('')
+watch(search, async (search) => {
+  // Only moderators can search other users
+  if (!isModerator.value) return
+  if (!search.length) {
+    searchOptions.value = []
+    return
+  }
+  const { data } = await userType.api.list({
+    meeting: meetingId.value,
+    search
+  })
+  searchOptions.value = [
+    ...usersToAutocomplete(data.filter(({ pk }) => user.value?.pk !== pk))
+  ]
+})
+
+const currentUserOptions = computed(() => [...userToAutocomplete(user.value!)])
+
+const selectedAuthorOption = computed(() =>
+  authorToAutocompleteItem(author.value)
+)
+
+const groupOptions = computed(() =>
+  postAsGroups.value.map(({ pk }) =>
+    authorToAutocompleteItem({
+      as_group: true,
+      author: user.value!.pk,
+      meeting_group: pk
+    })
+  )
+)
+
+function* skipSelected(
+  ...collection: AutocompleteItem[][]
+): Generator<AutocompleteItem> {
+  for (const items of collection) {
+    for (const item of items) {
+      if (item.value !== selectedAuthorOption.value.value) yield item
+    }
+  }
+}
+
+const options = computed(() => {
+  return [
+    selectedAuthorOption.value,
+    ...skipSelected(
+      currentUserOptions.value,
+      groupOptions.value,
+      searchOptions.value
+    )
+  ]
+})
 </script>
+
+<template>
+  <v-autocomplete
+    v-if="options"
+    :label="t('proposal.postAs')"
+    :items="options"
+    v-model="postAsSelect"
+    v-model:search="search"
+    :no-data-text="t('noSuggestions')"
+    hide-details
+    density="compact"
+  />
+</template>
