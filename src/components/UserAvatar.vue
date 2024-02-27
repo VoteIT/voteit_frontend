f24bb078918e89c745e34a71fc3b09fb7366a8a0
Johan Schiff <johan@betahaus.net>
2024-02-27 10:07:30 +0100

    update UserAvatar (vuetify disallows image + text now)

--- src/components/UserAvatar.vue
+++ src/components/UserAvatar.vue
@@ -2,29 +2,25 @@
   <UserPopup v-if="popup && computedUser" :user="computedUser">
     <template #activator="{ props }">
       <v-avatar
-        v-bind="{ ...$attrs, ...props }"
-        :size="size"
+        class="cursor-pointer"
         :color="bg"
         :image="image"
-        class="activator"
-      >
-        {{ initials }}
-      </v-avatar>
+        :size="size"
+        :text="initials"
+        v-bind="{ ...$attrs, ...props }"
+      />
     </template>
   </UserPopup>
-  <v-avatar v-else :size="size" :color="bg" :image="image">
-    {{ initials }}
-  </v-avatar>
+  <v-avatar v-else :color="bg" :image="image" :size="size" :text="initials" />
 </template>
 
 <script setup lang="ts">
-/* eslint-disable camelcase */
 import { computed } from 'vue'
 
-import useAuthentication from '@/composables/useAuthentication'
+import { ThemeColor } from '@/utils/types'
+import { user as currentUser } from '@/composables/useAuthentication'
 import useUserDetails from '@/modules/organisations/useUserDetails'
 import type { IUser } from '@/modules/organisations/types'
-import { ThemeColor } from '@/utils/types'
 
 import UserPopup from './UserPopup.vue'
 
@@ -42,9 +38,19 @@ const props = withDefaults(
 )
 
 const { getUser } = useUserDetails()
-const auth = useAuthentication()
+
 const computedUser = computed(
-  () => props.user ?? (props.pk ? getUser(props.pk) : auth.user.value)
+  () => props.user ?? (props.pk ? getUser(props.pk) : currentUser.value)
+)
+
+const bg = computed(() => {
+  if (!computedUser.value) return ThemeColor.Secondary
+  if (image.value) return
+  return props.color
+})
+
+const image = computed(
+  () => computedUser.value?.img_url ?? undefined // Change null to undefined
 )
 
 const initials = computed(() => {
@@ -56,20 +62,4 @@ const initials = computed(() => {
   if (!first_name && !last_name) return '?'
   return (first_name[0] + last_name[0]).toUpperCase()
 })
-
-const bg = computed(() => {
-  if (!computedUser.value) return ThemeColor.Secondary
-  if (image.value) return
-  return props.color
-})
-
-const image = computed(() => {
-  if (!computedUser.value) return
-  return computedUser.value.img_url ?? undefined // Change null to undefined
-})
 </script>
-
-<style lang="sass" scoped>
-.activator
-  cursor: pointer
-</style>
