dbd3e2de078c319ebc2d733549c3b5e01c427e80
Johan Schiff <johan@betahaus.net>
2025-03-07 11:33:19 +0100

    Add some spacing

--- src/modules/meetings/accessPolicies/Automatic.vue
+++ src/modules/meetings/accessPolicies/Automatic.vue
@@ -3,27 +3,26 @@
     <p class="mb-1">
       {{ $t('accessPolicies.automatic.joinDescription') }}
     </p>
-    <div class="mb-4">
+    <div class="mb-4 d-flex ga-1">
       <v-chip v-for="role in policy.roles_given" :key="role">
-        {{ meetingType.getRole(role).translateName(t) }}
+        {{ meetingType.getRole(role).translateName($t) }}
       </v-chip>
     </div>
     <div class="text-right">
       <v-btn
         color="primary"
-        :loading="submitting"
         :disabled="submitting"
+        :loading="submitting"
         prepend-icon="mdi-door-open"
-        @click="joinNow()"
-        >{{ $t('join.now') }}</v-btn
-      >
+        :text="$t('join.now')"
+        @click="joinNow"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { useI18n } from 'vue-i18n'
 import { useRouter } from 'vue-router'
 
 import useAlert from '@/composables/useAlert'
@@ -35,7 +34,6 @@ import { meetingType } from '../contentTypes'
 
 const props = defineProps<{ policy: AccessPolicy }>()
 
-const { t } = useI18n()
 const { alert } = useAlert()
 const router = useRouter()
 const { meetingRoute } = useMeeting()
