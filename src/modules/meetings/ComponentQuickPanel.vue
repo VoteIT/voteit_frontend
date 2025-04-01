08ddf92392985779ee17e93b1ae1c5069d8e73a1
Johan Schiff <johan@betahaus.net>
2025-04-01 13:50:51 +0200

    script / template order

--- src/modules/meetings/ComponentQuickPanel.vue
+++ src/modules/meetings/ComponentQuickPanel.vue
@@ -1,34 +1,3 @@
-<template>
-  <v-card-text v-if="isBlocked">
-    <em>
-      {{ $t('meeting.componentBlocked') }}
-    </em>
-  </v-card-text>
-  <v-card-text v-else>
-    <slot></slot>
-    <v-switch
-      v-if="!isBlocked"
-      color="primary"
-      hide-details
-      :label="switchLabel"
-      v-model="active"
-    />
-    <div v-if="isRequired" class="mt-2">
-      <v-chip>
-        <v-icon
-          :color="warnRequired ? 'warning' : 'secondary'"
-          :icon="warnRequired ? 'mdi-alert' : 'mdi-information'"
-          class="mr-2"
-        />
-        {{ $t('meeting.componentRequired') }}
-      </v-chip>
-    </div>
-  </v-card-text>
-  <v-card-actions v-if="$slots.actions && active">
-    <slot name="actions"></slot>
-  </v-card-actions>
-</template>
-
 <script setup lang="ts">
 import { computed } from 'vue'
 
@@ -66,3 +35,34 @@ const active = computed({
   }
 })
 </script>
+
+<template>
+  <v-card-text v-if="isBlocked">
+    <em>
+      {{ $t('meeting.componentBlocked') }}
+    </em>
+  </v-card-text>
+  <v-card-text v-else>
+    <slot></slot>
+    <v-switch
+      v-if="!isBlocked"
+      color="primary"
+      hide-details
+      :label="switchLabel"
+      v-model="active"
+    />
+    <div v-if="isRequired" class="mt-2">
+      <v-chip>
+        <v-icon
+          :color="warnRequired ? 'warning' : 'secondary'"
+          :icon="warnRequired ? 'mdi-alert' : 'mdi-information'"
+          class="mr-2"
+        />
+        {{ $t('meeting.componentRequired') }}
+      </v-chip>
+    </div>
+  </v-card-text>
+  <v-card-actions v-if="$slots.actions && active">
+    <slot name="actions"></slot>
+  </v-card-actions>
+</template>
