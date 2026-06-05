b0349532a59413295d4b18402500bc45b954af28
Johan Schiff <johan@betahaus.net>
2026-06-05 13:01:07 +0200

    Use VDataTable in RoleMatrix

--- src/modules/meetings/ParticipantsView.vue
+++ src/modules/meetings/ParticipantsView.vue
@@ -1,108 +1,3 @@
-<template>
-  <MeetingToolbar>
-    <v-spacer />
-    <v-tabs v-model="currentTab" :items="tabs" />
-  </MeetingToolbar>
-  <v-row>
-    <v-col>
-      <v-window v-model="currentTab" :touch="false">
-        <v-window-item value="default">
-          <RoleMatrix
-            :admin="!!canChangeRoles"
-            :cols="matrixCols"
-            :contentType="meetingType"
-            :filter="filterParticipants"
-            :icons="roleIcons"
-            :pk="meetingId"
-            :readonly-roles="readonlyRoles"
-            :remove-confirm="removeConfirm"
-            :remove-confirm-text="$t('meeting.confirmRemoveParticipant')"
-          >
-            <template #filter>
-              <div class="d-flex">
-                <v-text-field
-                  label="Sök"
-                  class="mr-1"
-                  clearable
-                  v-model="participantFilter.search"
-                />
-                <v-select
-                  :items="roleItems.slice(1)"
-                  class="ml-1"
-                  multiple
-                  label="Begränsa till roller"
-                  clearable
-                  v-model="participantFilter.roles"
-                />
-              </div>
-            </template>
-          </RoleMatrix>
-          <div v-if="canChangeRoles" class="d-flex flex-wrap mt-6">
-            <UserSearch
-              v-if="orgStore.isOrganisationManager"
-              :label="$t('meeting.addParticipant')"
-              class="flex-grow-1"
-              @submit="addUser"
-              :filter="searchFilter"
-            >
-              <template #hint>
-                {{ $t('invites.addUserInvitesWarning') }}
-              </template>
-            </UserSearch>
-            <div v-else class="flex-grow-1">
-              <i18n-t keypath="invites.invitesGoTo">
-                <template #link>
-                  <router-link
-                    to="participants"
-                    @click.prevent="currentTab = 'invites'"
-                  >
-                    {{ $t('invites.invites').toLocaleLowerCase() }}
-                  </router-link>
-                </template>
-              </i18n-t>
-            </div>
-            <v-menu>
-              <template #activator="{ props }">
-                <v-btn
-                  color="primary"
-                  prepend-icon="mdi-download"
-                  :text="$t('download')"
-                  variant="tonal"
-                  v-bind="props"
-                />
-              </template>
-              <v-list>
-                <v-list-item
-                  :href="getDownloadUrl('csv')"
-                  append-icon="mdi-file-download"
-                  :title="`${t('meeting.participants')} (CSV)`"
-                />
-                <v-list-item
-                  :href="getDownloadUrl('json')"
-                  append-icon="mdi-file-download"
-                  :title="`${t('meeting.participants')} (JSON)`"
-                />
-              </v-list>
-            </v-menu>
-          </div>
-        </v-window-item>
-
-        <v-window-item value="groups">
-          <MeetingGroupsTab />
-        </v-window-item>
-
-        <v-window-item value="speakerHistory" v-if="hasSpeakerSystems">
-          <SpeakerHistory />
-        </v-window-item>
-
-        <v-window-item value="invites" v-if="canViewMeetingInvite">
-          <InvitationsTab @denied="currentTab = 'default'" />
-        </v-window-item>
-      </v-window>
-    </v-col>
-  </v-row>
-</template>
-
 <script lang="ts" setup>
 import { computed, reactive } from 'vue'
 import { useI18n } from 'vue-i18n'
@@ -112,7 +7,6 @@ import restApi from '@/utils/restApi'
 import { openAlertEvent } from '@/utils/events'
 import UserSearch from '@/components/UserSearch.vue'
 import RoleMatrix from '@/components/RoleMatrix.vue'
-import { UserContextRoles } from '@/composables/types'
 import useTabRoute from '@/composables/useTabRoute'
 
 import useAuthStore from '../auth/useAuthStore'
@@ -134,13 +28,8 @@ import { roleIcons } from './utils'
 
 const { t } = useI18n()
 const authStore = useAuthStore()
-const {
-  meetingId,
-  meetingDialect,
-  canChangeRoles,
-  canViewMeetingInvite,
-  roleItems
-} = useMeeting()
+const { meetingId, meetingDialect, canChangeRoles, canViewMeetingInvite } =
+  useMeeting()
 const { getUserIds } = meetingType.useContextRoles()
 const { getUser } = useUserDetails(meetingId)
 const { hasSpeakerSystems } = useSpeakerSystems(meetingId)
@@ -226,14 +115,125 @@ const participantFilter = reactive<{
   roles: [],
   search: null
 })
-function filterParticipants({ user, assigned }: UserContextRoles) {
+function filterParticipants(_value: unknown, _query: string, item: any) {
   const { roles, search } = participantFilter
   if (search) {
-    const u = getUser(user)
+    const u = getUser(item.raw.user)
     if (!u) return false
     const joined = `${getFullName(u)} ${u.email}`.toLocaleLowerCase()
     if (!joined.includes(search.toLocaleLowerCase())) return false
   }
-  return roles.every((r) => assigned.has(r))
+  return roles.every((r) => item.raw[r] === true)
 }
 </script>
+
+<template>
+  <MeetingToolbar>
+    <v-spacer />
+    <v-tabs v-model="currentTab" :items="tabs" />
+  </MeetingToolbar>
+  <v-row>
+    <v-col>
+      <v-window v-model="currentTab" :touch="false">
+        <v-window-item value="default">
+          <RoleMatrix
+            :admin="!!canChangeRoles"
+            :cols="matrixCols"
+            :contentType="meetingType"
+            :filter="filterParticipants"
+            :icons="roleIcons"
+            :pk="meetingId"
+            :readonly-roles="readonlyRoles"
+            :remove-confirm="removeConfirm"
+            :remove-confirm-text="$t('meeting.confirmRemoveParticipant')"
+          >
+            <template #filter="{ columnDescriptions }">
+              <div class="d-flex ga-1">
+                <v-text-field
+                  label="Sök"
+                  class="w-50"
+                  clearable
+                  v-model="participantFilter.search"
+                />
+                <v-select
+                  chips
+                  class="w-50"
+                  clearable
+                  :items="
+                    columnDescriptions.map((cd) => ({
+                      value: cd.name,
+                      title: cd.title
+                    }))
+                  "
+                  label="Begränsa till roller"
+                  multiple
+                  v-model="participantFilter.roles"
+                />
+              </div>
+            </template>
+          </RoleMatrix>
+          <div v-if="canChangeRoles" class="d-flex flex-wrap mt-6">
+            <UserSearch
+              v-if="orgStore.isOrganisationManager"
+              :label="$t('meeting.addParticipant')"
+              class="flex-grow-1"
+              @submit="addUser"
+              :filter="searchFilter"
+            >
+              <template #hint>
+                {{ $t('invites.addUserInvitesWarning') }}
+              </template>
+            </UserSearch>
+            <div v-else class="flex-grow-1">
+              <i18n-t keypath="invites.invitesGoTo">
+                <template #link>
+                  <router-link
+                    to="participants"
+                    @click.prevent="currentTab = 'invites'"
+                  >
+                    {{ $t('invites.invites').toLocaleLowerCase() }}
+                  </router-link>
+                </template>
+              </i18n-t>
+            </div>
+            <v-menu>
+              <template #activator="{ props }">
+                <v-btn
+                  color="primary"
+                  prepend-icon="mdi-download"
+                  :text="$t('download')"
+                  variant="tonal"
+                  v-bind="props"
+                />
+              </template>
+              <v-list>
+                <v-list-item
+                  :href="getDownloadUrl('csv')"
+                  append-icon="mdi-file-download"
+                  :title="`${t('meeting.participants')} (CSV)`"
+                />
+                <v-list-item
+                  :href="getDownloadUrl('json')"
+                  append-icon="mdi-file-download"
+                  :title="`${t('meeting.participants')} (JSON)`"
+                />
+              </v-list>
+            </v-menu>
+          </div>
+        </v-window-item>
+
+        <v-window-item value="groups">
+          <MeetingGroupsTab />
+        </v-window-item>
+
+        <v-window-item value="speakerHistory" v-if="hasSpeakerSystems">
+          <SpeakerHistory />
+        </v-window-item>
+
+        <v-window-item value="invites" v-if="canViewMeetingInvite">
+          <InvitationsTab @denied="currentTab = 'default'" />
+        </v-window-item>
+      </v-window>
+    </v-col>
+  </v-row>
+</template>
