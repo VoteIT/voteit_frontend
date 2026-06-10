70925c4633580b8bfd79fa8ca90e9ab7e02343c5
Johan Schiff <johan@betahaus.net>
2026-06-10 12:33:10 +0200

    Unused types

--- src/contentTypes/types.ts
+++ src/contentTypes/types.ts
@@ -18,25 +18,6 @@ export type ConditionalWorkflowStates<T extends { state?: string }> =
       }
     : never
 
-export interface TransitionCondition {
-  name: string
-  allowed: boolean // Condition met?
-  title: string // Title or name, mostly meant for the developer
-}
-
-// Transitions from backend
-export interface ITransition<Event extends string = string> {
-  name: Event
-  permission: string
-  source: string
-  target: string
-  title: string
-  icon?: string
-  conditions: TransitionCondition[]
-  allowed: boolean // perm + conditions met?
-  has_perm: boolean // Is the permission met?
-}
-
 export interface BaseContent {
   readonly pk: number
   title: string
