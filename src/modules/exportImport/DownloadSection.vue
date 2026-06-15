3be256c32b80966d92154b6d71b4338eb09d28d1
Johan Schiff <johan@betahaus.net>
2026-06-15 16:10:08 +0200

    Meeting yaml export w options (wip)

---
+++ src/modules/exportImport/DownloadSection.vue
@@ -0,0 +1,97 @@
+<script lang="ts" setup>
+import { sorted } from 'itertools'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import { meetingExportPlugins } from './registry'
+import useMeeting from '../meetings/useMeeting'
+
+const { t } = useI18n()
+
+const { meeting, meetingId } = useMeeting()
+
+function* iterDownloads(
+  defaultTitle: string,
+  exports: { title?: string; formats: { format: string; url: string }[] }[]
+) {
+  for (const { title, formats } of exports) {
+    for (const { format, url } of formats) {
+      yield {
+        title: `${title || defaultTitle} (${format.toUpperCase()})`,
+        url
+      }
+    }
+  }
+}
+
+const exportPlugins = computed(() => {
+  if (!meeting.value) return []
+  const plugins = meetingExportPlugins.getActivePlugins(meeting.value)
+  return sorted(
+    plugins
+      .map(({ id, getExports, getTitle }) => {
+        const title = getTitle(t)
+        return {
+          id,
+          exports: [...iterDownloads(title, getExports(meetingId.value))],
+          title
+        }
+      })
+      .filter((e) => e.exports.length),
+    (o) => o.title.toLocaleLowerCase()
+  )
+})
+</script>
+
+<template>
+  <div>
+    <header class="mb-6">
+      <h2>
+        {{ $t('exportImport.downloadFromMeeting') }}
+      </h2>
+      <i18n-t keypath="exportImport.exportsLongDescription">
+        <template #csv>
+          <a
+            href="https://en.wikipedia.org/wiki/Comma-separated_values"
+            target="_blank"
+            v-text="'CSV'"
+          />
+        </template>
+        <template #json>
+          <a href="https://en.wikipedia.org/wiki/JSON" target="_blank">JSON</a>
+        </template>
+      </i18n-t>
+    </header>
+    <v-list class="my-2" rounded>
+      <v-list-item
+        v-for="{ exports, id, title } in exportPlugins"
+        :key="id"
+        :title="title"
+      >
+        <template #append>
+          <v-menu location="bottom right">
+            <template #activator="{ props }">
+              <v-btn
+                append-icon="mdi-chevron-down"
+                color="primary"
+                :text="$t('download')"
+                variant="flat"
+                v-bind="props"
+              />
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="{ title, url } in exports"
+                :key="url"
+                append-icon="mdi-download"
+                target="_blank"
+                :href="url"
+                :title="title"
+              />
+            </v-list>
+          </v-menu>
+        </template>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
--- src/modules/exportImport/ExportSection.vue
+++ src/modules/exportImport/ExportSection.vue
@@ -1,97 +1,171 @@
 <script lang="ts" setup>
-import { sorted } from 'itertools'
-import { computed } from 'vue'
-import { useI18n } from 'vue-i18n'
+import { computed, shallowReactive, type ComputedRef } from 'vue'
+import { ComposerTranslation, useI18n } from 'vue-i18n'
 
-import { meetingExportPlugins } from './registry'
-import useMeeting from '../meetings/useMeeting'
+import useRules from '@/composables/useRules'
+import { getApiLink } from '@/utils/restApi'
+import useMeetingId from '../meetings/useMeetingId'
 
-const { t } = useI18n()
-
-const { meeting, meetingId } = useMeeting()
-
-function* iterDownloads(
-  defaultTitle: string,
-  exports: { title?: string; formats: { format: string; url: string }[] }[]
-) {
-  for (const { title, formats } of exports) {
-    for (const { format, url } of formats) {
-      yield {
-        title: `${title || defaultTitle} (${format.toUpperCase()})`,
-        url
-      }
-    }
-  }
+interface ExportOption {
+  key: string
+  default: boolean
+  translate(t: ComposerTranslation): string
 }
 
-const exportPlugins = computed(() => {
-  if (!meeting.value) return []
-  const plugins = meetingExportPlugins.getActivePlugins(meeting.value)
-  return sorted(
-    plugins
-      .map(({ id, getExports, getTitle }) => {
-        const title = getTitle(t)
-        return {
-          id,
-          exports: [...iterDownloads(title, getExports(meetingId.value))],
-          title
-        }
-      })
-      .filter((e) => e.exports.length),
-    (o) => o.title.toLocaleLowerCase()
+const exportOptions: ExportOption[] = [
+  // THESE ARE IMPORT OPTIONS
+  // {
+  //   key: 'add_participants',
+  //   default: false,
+  //   translate: (t) => t('exportImport.options.addParticipants')
+  // },
+  // {
+  //   key: 'use_existing_groups',
+  //   default: false,
+  //   translate: (t) => t('exportImport.options.useExistingGroups')
+  // },
+  {
+    key: 'clear_group_authors',
+    default: false,
+    translate: (t) => t('exportImport.options.clearGroupAuthors')
+  },
+  {
+    key: 'clear_authors',
+    default: false,
+    translate: (t) => t('exportImport.options.clearAuthors')
+  },
+  {
+    key: 'clear_ai_states',
+    default: false,
+    translate: (t) => t('exportImport.options.clearAiStates')
+  },
+  {
+    key: 'clear_proposal_states',
+    default: false,
+    translate: (t) => t('exportImport.options.clearProposalStates')
+  },
+  {
+    key: 'clear_proposal_id',
+    default: false,
+    translate: (t) => t('exportImport.options.clearProposalId')
+  },
+  {
+    key: 'include_groups',
+    default: true,
+    translate: (t) => t('meeting.groups.groups')
+  },
+  {
+    key: 'include_proposals',
+    default: true,
+    translate: (t) => t('proposal.proposals')
+  },
+  {
+    key: 'include_discussions',
+    default: true,
+    translate: (t) => t('discussion.discussion')
+  },
+  {
+    key: 'include_buttons',
+    default: true,
+    translate: (t) => t('reaction.buttons')
+  },
+  {
+    key: 'include_reactions',
+    default: false,
+    translate: (t) => t('reaction.peopleReacted')
+  }
+]
+
+const { t } = useI18n()
+const rules = useRules(t)
+
+const options = computed(() =>
+  exportOptions.map((option) => ({
+    ...option,
+    title: option.translate(t)
+  }))
+)
+
+const values = shallowReactive<Record<string, boolean>>(
+  Object.fromEntries(
+    exportOptions.map((option) => [option.key, option.default])
   )
-})
+)
+
+const clearOptions = computed(() =>
+  options.value.filter((option) => option.key.startsWith('clear_'))
+)
+
+const includeOptions = computed(() =>
+  options.value.filter((option) => option.key.startsWith('include_'))
+)
+
+function selectionModel(group: ComputedRef<{ key: string }[]>) {
+  return computed<string[]>({
+    get: () =>
+      group.value.filter((option) => values[option.key]).map((o) => o.key),
+    set: (keys) => {
+      for (const option of group.value) {
+        values[option.key] = keys.includes(option.key)
+      }
+    }
+  })
+}
+
+const clearSelected = selectionModel(clearOptions)
+const includeSelected = selectionModel(includeOptions)
+
+const meetingId = useMeetingId()
+
+function download() {
+  const params = new URLSearchParams()
+  for (const [key, enabled] of Object.entries(values)) {
+    if (enabled) params.set(key, 'true')
+  }
+  const url = getApiLink(`meeting-data/${meetingId.value}/yaml?${params}`)
+  window.open(url, '_blank')
+}
 </script>
 
 <template>
   <div>
     <header class="mb-6">
       <h2>
-        {{ $t('exportImport.exports') }}
+        {{ $t('exportImport.exportFromMeeting') }}
       </h2>
-      <i18n-t keypath="exportImport.exportsLongDescription">
-        <template #csv>
-          <a
-            href="https://en.wikipedia.org/wiki/Comma-separated_values"
-            target="_blank"
-            >CSV</a
-          >
-        </template>
-        <template #json>
-          <a href="https://en.wikipedia.org/wiki/JSON" target="_blank">JSON</a>
-        </template>
-      </i18n-t>
+      <p>{{ $t('exportImport.exportFromMeetingDescription') }}</p>
     </header>
-    <v-list class="my-2" rounded>
-      <v-list-item
-        v-for="{ exports, id, title } in exportPlugins"
-        :key="id"
-        :title="title"
-      >
-        <template #append>
-          <v-menu location="bottom right">
-            <template #activator="{ props }">
-              <v-btn
-                append-icon="mdi-chevron-down"
-                color="primary"
-                :text="$t('download')"
-                variant="flat"
-                v-bind="props"
-              />
-            </template>
-            <v-list>
-              <v-list-item
-                v-for="{ title, url } in exports"
-                :key="url"
-                append-icon="mdi-download"
-                target="_blank"
-                :href="url"
-                :title="title"
-              />
-            </v-list>
-          </v-menu>
-        </template>
-      </v-list-item>
-    </v-list>
+    <v-form @submit.prevent="download" v-slot="{ isValid }">
+      <v-select
+        v-model="includeSelected"
+        :items="includeOptions"
+        item-title="title"
+        item-value="key"
+        :label="$t('exportImport.options.includeLabel')"
+        :rules="[rules.required]"
+        multiple
+        chips
+        closable-chips
+      />
+      <v-select
+        v-model="clearSelected"
+        :items="clearOptions"
+        item-title="title"
+        item-value="key"
+        :label="$t('exportImport.options.clearLabel')"
+        multiple
+        chips
+        closable-chips
+      />
+      <div class="text-right">
+        <v-btn
+          color="primary"
+          :disabled="!isValid.value"
+          prepend-icon="mdi-download"
+          :text="$t('exportImport.exportFromMeeting')"
+          type="submit"
+        />
+      </div>
+    </v-form>
   </div>
 </template>
