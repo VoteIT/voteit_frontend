f3cffbf358618230e17fc8dd9405fb9395167e61
Johan Schiff <johan@betahaus.net>
2026-06-17 12:31:23 +0200

    Disable import on started meetings

--- src/modules/exportImport/ControlPanel.vue
+++ src/modules/exportImport/ControlPanel.vue
@@ -1,13 +1,34 @@
 <script lang="ts" setup>
+import { computed } from 'vue'
+
+import useMeetingId from '../meetings/useMeetingId'
+import useMeetingStore from '../meetings/useMeetingStore'
+import { MeetingState } from '../meetings/types'
+
 import ImportSection from './ImportSection.vue'
 import ExportSection from './ExportSection.vue'
 import DownloadSection from './DownloadSection.vue'
+
+const meetingId = useMeetingId()
+const meetingStore = useMeetingStore()
+
+const isUpcoming = computed(
+  () =>
+    meetingStore.getMeeting(meetingId.value)?.state === MeetingState.Upcoming
+)
 </script>
 
 <template>
   <div>
     <h1 class="mb-6">{{ $t('exportImport.importAndExport') }}</h1>
-    <ImportSection class="mb-8" />
+    <ImportSection v-if="isUpcoming" class="mb-8" />
+    <v-alert
+      v-else
+      class="mb-8"
+      type="info"
+      variant="tonal"
+      :text="$t('exportImport.importOnlyUpcoming')"
+    />
     <v-divider class="my-6" />
     <ExportSection />
     <v-divider class="my-6" />
